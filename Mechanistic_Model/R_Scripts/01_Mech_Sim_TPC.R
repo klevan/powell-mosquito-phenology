705c60494905731715fd85790d5c25076dd3e32d
tmcdevittgalles <tmcdevitt-galles@usgs.gov>
2021-05-17 13:42:44 -0600

    cleaning code and adding early figures

--- Mechanistic_Model/R_Scripts/01_Mech_Sim_TPC.R
+++ Mechanistic_Model/R_Scripts/01_Mech_Sim_TPC.R
@@ -36,14 +36,17 @@ return(perform)
     
 }
 
+## testing the TPC function
+temp <- seq(-10, 20, length.out = 100) # initial temperature range
 
-temp <- seq(0, 20, length.out = 100)
-at1 <- Dev_TPC( Temp = temp, Toptim = 14, CTmax= 25, sigma = 2.5)
+at1 <- Dev_TPC( Temp = temp, Toptim = 18, CTmax= 30, sigma = 1.5)
 
-plot(x= temp, y= at1/5)
+# plotting the thermal performance curve
 
+plot(x= temp, y= at1/3)
+# doesn't look that good but we can carry on with it
 
-### Extracting temp data ###
+### Extracting temp data from PRISM, use this for our seasonal model ###
 
 ## setting wd to upload data
 setwd("C:/Users/tmcdevitt-galles/powell-mosquito-phenology")
@@ -64,12 +67,10 @@ load("./Data/combinded.Rda")
 
 site.df <- unique(select(ungroup(complete.df), c("Site", "Plot")))
 
-
-
+# Need to add Site info to prism data 
 contig.df <- left_join(contigus.df, site.df , by="Plot")
 
-
-focal.df <- filter( contig.df, Site=="WOOD")
+focal.df <- filter( contig.df, Site=="WOOD") # modeling wood from D9
 
 focal.df$fYear <- as.factor(focal.df$Year)
 
@@ -91,12 +92,11 @@ colnames(dum.df) <- c("DOY", "fYear")
 
 dum.df <- as.data.frame(dum.df)
 
-
 dum.df <- tidyr::expand( dum.df, nesting(DOY, fYear),plot.m)
 
 dum.df$Pred <- predict(gam.temp$gam, newdata = dum.df)
 
-
+# Looking at predicted number
 dum.df %>% filter( fYear != "2013") %>% 
   ggplot(  aes(x=DOY, y=(Pred)))+ 
   geom_point(data=focal.df, aes(x= DOY,y=Tmean7, color=fYear),size=2)+
@@ -104,31 +104,41 @@ dum.df %>% filter( fYear != "2013") %>%
   geom_line(size=2,alpha=.75,color="black")+ theme_classic()+ 
   facet_wrap(~fYear)
 
-
+# putting predicted temp data to new data set
 temp.df <- dum.df
 
+# Adding improved col names
 colnames(temp.df) <- c("DOY", "fYear", "Plot", "Pred")
 
-temp.df$Metric <- "Temp"
-
+# selecting col. of interest
 pred.df <- select(temp.df, c("DOY", "Pred", "fYear"))
 
+# filtering out so we just have one year of temp data
 pred.df <- unique(pred.df %>% filter(fYear == "2017"))
 
+# renaming column
 colnames(pred.df)[2] <- "Temp"
 
+# filtering to better start with when mosquitoes are present
+pred.df <- pred.df %>%  filter(DOY>=100)
+
+# adding columns to track JUV, and adults
 pred.df$Juv <- NA
 pred.df$Adult <- NA
 
+# column to track changes in development rate
+pred.df$DevRate <-NA
 
 ## Initial parameters
 
-pred.df$Juv[1] <- 100 ## overwinter larval population
+pred.df$Juv[1] <- 1000 ## overwinter larval population
 pred.df$Adult[1] <- 0 ## number of adults in day 1 should be 0
 
-fec <- 1.2 ## setting a fecudity rate of 3 eggs per day per female
+
+fec <- 1.02 ## setting a fecudity rate of 3 eggs per day per female
 aMortal <- 0.25 ## setting mortality rate for adults
-lMortal <- .18 ## setting mortality rate for larvae
+lMortal <- .2 ## setting mortality rate for larvae
+
 
 ## Lets run the model
 
@@ -136,14 +146,58 @@ for( i in 1:(nrow(pred.df)-1)){
   ## Larval dynamics
   pred.df$Juv[i+1] <- pred.df$Juv[i]+ (pred.df$Adult[i] * fec) -
                       pred.df$Juv[i] * lMortal - 
-                      pred.df$Juv[i] * (Dev_TPC( pred.df$Temp[i], Toptim = 18,
-                                                 CTmax=30, sigma = 2.5)/8)
+                      pred.df$Juv[i] * (Dev_TPC( pred.df$Temp[i],  Toptim =15,
+                                                 CTmax= 25, sigma = 1.5)/2)
+  pred.df$DevRate[i] <- Dev_TPC( pred.df$Temp[i],  Toptim = 15,
+                                 CTmax=25, sigma = 1.5)
   ## Adult dynamics
   pred.df$Adult[i+1] <- pred.df$Adult[i] - (pred.df$Adult[i] * aMortal) +
-    pred.df$Juv[i] *  (Dev_TPC( Temp = pred.df$Temp[i], Toptim = 18, 
-                                CTmax= 30, sigma = 2.5)/8)
+    pred.df$Juv[i] *  (Dev_TPC( Temp = pred.df$Temp[i], Toptim = 15, 
+                                CTmax= 25, sigma = 1.5)/4)
 }
 
-pred.df %>% filter(DOY > 100) %>% 
-ggplot( aes( x=DOY, y=Adult)) + geom_point() +
-  geom_point( aes(x =DOY, y= Juv), color= "grey")
+pred.df %>% filter(DOY >= 150) %>% 
+ggplot( aes( x=DOY, y=Adult)) + geom_line(size=2, alpha=.85,
+                                          aes(color="black")) +
+  geom_line(aes(x=DOY, y= Juv, color="grey"), size=2,alpha=.75) +
+  ylab("Mosquito abundance") + theme_classic()+
+  scale_color_manual(name="Stage", values=c("black" = "black","grey"="grey"),
+                     labels=c("Adult", "Larvae"))+
+  theme( legend.key.size = unit(.5, "cm"),
+         legend.title =element_text(size=14,margin = margin(r =10, unit = "pt")),
+         legend.text=element_text(size=14,margin = margin(r =10, unit = "pt")), 
+         legend.position = c(.9,.9),
+         axis.line.x = element_line(color="black") ,
+         axis.ticks.y = element_line(color="black"),
+         axis.ticks.x = element_line(color="black"),
+         axis.title.x = element_text(size = rel(1.8)),
+         axis.text.x  = element_text(vjust=0.5, color = "black"),
+         axis.text.y  = element_text(vjust=0.5,color = "black"),
+         axis.title.y = element_text(size = rel(1.8), angle = 90) ,
+         strip.text.x = element_text(size=20) )
+
+
+
+pred.df %>% 
+  ggplot( aes( x=DOY, y=DevRate)) + geom_line(size=2, alpha=.85,
+                                              aes(color="black")) +
+  geom_line(aes(x=DOY, y= Temp/max(Temp), color="grey"), size=2,alpha=.75) +
+  scale_y_continuous( sec.axis = sec_axis(~. * max(pred.df$Temp),
+                                          name="Mean Temp (C)"),
+                      name="Relative Development Rate") + theme_classic()+
+  scale_color_manual(name="", values=c("black" = "black","grey"="grey"),
+                     labels=c("Devel. rate", "Temp."))+
+  theme( legend.key.size = unit(.5, "cm"),
+         legend.title =element_text(size=14,margin = margin(r =10, unit = "pt")),
+         legend.text=element_text(size=14,margin = margin(r =10, unit = "pt")), 
+         legend.position = c(.9,.9),
+         axis.line.x = element_line(color="black") ,
+         axis.ticks.y = element_line(color="black"),
+         axis.ticks.x = element_line(color="black"),
+         axis.title.x = element_text(size = rel(1.8)),
+         axis.text.x  = element_text(vjust=0.5, color = "black"),
+         axis.text.y  = element_text(vjust=0.5,color = "black"),
+         axis.title.y = element_text(size = rel(1.8), angle = 90) ,
+         strip.text.x = element_text(size=20) )
+
+             
